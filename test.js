342e6ddcf58a9a6036006a2a3c168d5affca74eb
mtkramer <kramerm1@mail.usf.edu>
2020-04-24 10:04:19 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+/*
+* ROADMAP
+* create git repo
+* setup the project
+* push new project to repo
+* structure the html tags
+* pick the topic of the story for the page
+* format the story as is required by instructions
+* finish the 'story'...
+*/
